f7b6304ae956389a2feac481edcd72cd6aac3650
estersr <estherramoos@gmail.com>
2024-05-22 14:44:58 -0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Esther Ramos on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField ("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker ("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text ("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Esther Ramos on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
